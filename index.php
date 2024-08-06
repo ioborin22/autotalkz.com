dd84d3b8eb5f6cb800ab6a1a18fdfd5310cf0496
ioborin22 <ioborin22@gmail.com>
2024-08-06 13:10:55 -0400

    20240806

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 include_once $_SERVER['DOCUMENT_ROOT']."/system/main.php";
 
 if(isset($statya)){
