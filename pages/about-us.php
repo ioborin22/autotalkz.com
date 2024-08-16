a5ae491915f7aa1633ea7634537f207b3bb99ce5
ioborin22 <ioborin22@gmail.com>
2024-08-16 10:03:22 -0400

    20240816

---
+++ pages/about-us.php
@@ -0,0 +1,8 @@
+<?php include($_SERVER['DOCUMENT_ROOT'].'/system/2.php'); ?>
+<section>
+    <div class="content">
+        <h1>About Us</h1>
+        <p>Welcome to AutoTalkz! We are dedicated to providing the best information about cars, their history, and specifications. Our team of experts and enthusiasts works tirelessly to bring you accurate and comprehensive reviews on the latest and most popular car models.</p>
+    </div>
+</section>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/system/3.php'); ?>
